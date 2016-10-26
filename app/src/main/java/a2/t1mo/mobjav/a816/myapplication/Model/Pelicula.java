1293d1e9f24c60b8894945941c04d5a2b6a139fb
Unknown <paulaalva@gmail.com>
2016-10-26 12:13:12 -0300

    MVC

---
+++ app/src/main/java/a2/t1mo/mobjav/a816/myapplication/Model/Pelicula.java
@@ -0,0 +1,7 @@
+package a2.t1mo.mobjav.a816.myapplication.Model;
+
+/**
+ * Created by polialva on 26/10/16.
+ */
+public class Pelicula {
+}
--- app/src/main/java/a2/t1mo/mobjav/a816/myapplication/View/MainActivity.java
+++ app/src/main/java/a2/t1mo/mobjav/a816/myapplication/View/MainActivity.java
@@ -1,7 +1,9 @@
-package a2.t1mo.mobjav.a816.myapplication;
+package a2.t1mo.mobjav.a816.myapplication.View;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import a2.t1mo.mobjav.a816.myapplication.R;
 
 public class MainActivity extends AppCompatActivity {
 
