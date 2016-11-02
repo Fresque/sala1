5e3fc3d006cf9179d801e4f16c2249d772323b47
Fresque <martin11_29@outlook.com>
2016-11-02 00:45:07 -0300

    Descarga y muestra
    La app descarga una lista de peliculas populares de TMDB, y muestra en la pantalla principal el poster.
    20 pelis descargadas.

---
+++ app/src/main/java/a2/t1mo/mobjav/a816/myapplication/Util/HTTPConnectionManager.java
@@ -0,0 +1,44 @@
+package a2.t1mo.mobjav.a816.myapplication.Util;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.SocketTimeoutException;
+import java.net.URL;
+import java.net.UnknownHostException;
+
+/**
+ * Created by marti on 10/28/2016.
+ */
+
+public class HTTPConnectionManager {
+
+    public InputStream getRequestStream(String url) throws Exception{
+        InputStream result = null;
+        try {
+            //GENERA UNA CONEXION ENTRE MI APLICACION Y EL SERVIDOR WEB DE ESA URL
+            HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
+
+            //ESPERA MAXIMA
+            connection.setReadTimeout(10000);
+
+            //TIPO DE REQUEST
+            connection.setRequestMethod("GET");
+
+            //LEE EL CONTENIDO QUE DESCARGO
+            result =  connection.getInputStream();
+
+        }catch (SocketTimeoutException e) {
+            e.printStackTrace();
+            throw new SocketTimeoutException("Connection timeout. Please try again later.");
+        } catch (UnknownHostException e) {
+            throw new UnknownHostException("There is no internet connection");
+        } catch (IOException e) {
+            throw new IOException("Connection Problem");
+        }
+        return result;
+    }
+
+
+
+}
---
+++ app/src/main/java/a2/t1mo/mobjav/a816/myapplication/Util/ResultListener.java
@@ -0,0 +1,8 @@
+package a2.t1mo.mobjav.a816.myapplication.Util;
+
+/**
+ * Created by marti on 11/1/2016.
+ */
+
+public interface ResultListener {
+}
