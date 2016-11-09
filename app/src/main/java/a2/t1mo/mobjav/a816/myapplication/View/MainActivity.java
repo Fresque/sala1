7099b32eb462003250845fbd2581d5a60d9a5b9b
Unknown <paulaalva@gmail.com>
2016-11-09 13:29:36 -0300

    nav

--- app/src/main/java/a2/t1mo/mobjav/a816/myapplication/View/MainActivity.java
+++ app/src/main/java/a2/t1mo/mobjav/a816/myapplication/View/MainActivity.java
@@ -1,41 +1,31 @@
 package a2.t1mo.mobjav.a816.myapplication.View;
 
 
-import android.app.ActionBar;
-import android.support.design.widget.TabLayout;
-import android.support.v4.app.FragmentActivity;
-import android.support.v4.view.ViewPager;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Toast;
+import android.support.v7.app.AppCompatActivity;
 
-import a2.t1mo.mobjav.a816.myapplication.Model.Pelicula;
 import a2.t1mo.mobjav.a816.myapplication.R;
 
-public class MainActivity extends FragmentActivity {
+public class MainActivity extends AppCompatActivity {
 
-    //ADAPTER VIEWPAGER
-    MoviesViewPagerAdapter moviesViewPagerAdapter;
-    ViewPager mViewPager;
-    TabLayout tabLayout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final ActionBar actionBar = getActionBar();
+        //final ActionBar actionBar = getActionBar();
 
-        // SETEA EL VIEW PAGER QUE EN UN FUTURO VA A CONTENER LOS DIFERENTES RECYCLERS
-        moviesViewPagerAdapter = new MoviesViewPagerAdapter(getSupportFragmentManager());
+        ViewPagerFragment viewPagerFragment = new ViewPagerFragment();
 
-        mViewPager = (ViewPager)findViewById(R.id.viewPagerPrincipal);
-        mViewPager.setAdapter(moviesViewPagerAdapter);
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.acaVaElFragment, viewPagerFragment);
+        fragmentTransaction.commit();
 
-        //SETEO EL TABlAYOUT AL VIEWPAGER
-        //FALTA ESTABLECER COLORES Y DEMAS
-        tabLayout = (TabLayout) findViewById(R.id.tabLayout);
-        tabLayout.setupWithViewPager(mViewPager);
     }
 
 }
